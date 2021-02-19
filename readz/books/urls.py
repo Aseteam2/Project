615c37480af5b489f5d3a5c73b17ca6936f25ca2
Anmolkhaira13 <79054103+Anmolkhaira13@users.noreply.github.com>
2021-02-19 11:01:43 +0530

    django setup
    django setup

--- readz/books/urls.py
+++ readz/books/urls.py
@@ -13,17 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 from books import views
-from django.conf import settings
-from django.contrib.staticfiles.urls import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
 
 urlpatterns = [
-   path('books/', include('books.urls')),
-   path('admin/', admin.site.urls),
-   path('home/', views.home, name = 'home'),
-   path('login/', views.usr_login, name = 'login'),
+   path('', views.home, name = 'home'),
+   path('', views.usr_login, name = 'login'),
 ]
--- readz/books/views.py
+++ readz/books/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
+from books.forms import UserForm
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
+from . import forms
+from datetime import datetime
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    dict1={}
+    return render(request,"homepage.html",context=dict1)
+def usr_login(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request,user)
+                args={'user':request.user}
+                return render(request,'homepage.html',args)
+            else:
+                return HttpResponse("Account is not active")
+
+        else:
+            print("someone else tried login")
+            print("username :{} and password {}".format(username,password))
+            return HttpResponse("Invalid Details Provided")
+
+    else:
+        return render(request, 'login.html',{})                    
+
+def register(request):
+    registered=False
+    if request.method=="POST":
+        user_form=UserForm(data=request.POST)
+        if user_form.is_valid() :
+            user=user_form.save()
+            user.set_password(user.password)
+            user.save()
+            return render(request, 'login.html')
+            
+        else:
+            print(user_form.errors)   
+    else: 
+        user_form=UserForm()        
+    return render(request,"register.html",{'user_form':user_form,registered:'registered'})
