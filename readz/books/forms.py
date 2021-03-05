7c740de807deb47b954bc88fe1413cc9a8360153
1satwindersingh <singhsatwinder4056@gmail.com>
2021-03-05 20:04:19 +0530

    Added Validations to forms

--- readz/books/forms.py
+++ readz/books/forms.py
@@ -3,9 +3,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import RegexValidator
 from django.contrib.auth.password_validation import NumericPasswordValidator
+from django.contrib.auth.password_validation import validate_password
+
 
 class UserForm(forms.ModelForm):
-    password=forms.CharField(widget=forms.PasswordInput())
+    password = forms.CharField(widget = forms.PasswordInput(), validators=[validate_password] )
+    confirm_password = forms.CharField(widget = forms.PasswordInput(), validators=[validate_password] )
+    #email = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Email'}))
     class Meta():
         model =User
-        fields =('username', 'email', 'password')
+        fields =('username', 'email', 'password', 'confirm_password')
+
+    def clean(self):
+        cleaned_data = super(UserForm, self).clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "password and confirm_password does not match"
+            )
